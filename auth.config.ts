cdf7bc5705970794bc75a578e04cc2a2c476e5ce
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2025-02-13 09:13:46 -0400

    Se configuró la configuración para el login de authjs con usuario y contraseña

--- auth.config.ts
+++ auth.config.ts
@@ -1,11 +1,34 @@
+import Credentials from "@auth/core/providers/credentials";
 import GitHub from "@auth/core/providers/github";
+import { db, eq, User } from "astro:db";
 import { defineConfig } from "auth-astro";
+import bcrypt from "bcryptjs";
 
 export default defineConfig({
   providers: [
-    GitHub({
-      clientId: import.meta.env.GITHUB_CLIENT_ID,
-      clientSecret: import.meta.env.GITHUB_CLIENT_SECRET,
+    Credentials({
+      credentials: {
+        email: { label: "Correo electrónico", type: "email" },
+        password: { label: "Contraseña", type: "password" },
+      },
+      authorize: async ({ email, password }) => {
+        const [user] = await db
+          .select()
+          .from(User)
+          .where(eq(User.email, email as string));
+
+        if (!user) {
+          throw new Error("User not found");
+        }
+
+        if (!bcrypt.compareSync(password as string, user.password)) {
+          throw new Error("Invalid password");
+        }
+
+        const { password: _, ...rest } = user;
+
+        return rest;
+      },
     }),
   ],
 });
