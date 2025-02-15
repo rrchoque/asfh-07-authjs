0fc89da0b955aea5385db28c07874098cff89b03
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2025-02-15 05:39:48 -0400

    Se crearon las tablas de producto e images y el seed de estas tablas

--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,7 @@
-import { db, Role, User } from "astro:db";
+import { db, Role, User, Product, ProductImage } from "astro:db";
 import { v4 as UUID } from "uuid";
 import bcrypt from "bcryptjs";
+import { seedProducts } from "./seed-data";
 
 // https://astro.build/db/seed
 export default async function seed() {
@@ -35,4 +36,36 @@ export default async function seed() {
 
   await db.insert(Role).values(roles);
   await db.insert(User).values([reynaldo, johnDoe, janeDoe]);
+
+  const queries: any = [];
+
+  seedProducts.forEach((p) => {
+    const product = {
+      id: UUID(),
+      description: p.description,
+      gender: p.gender,
+      price: p.price,
+      sizes: p.sizes.join(","),
+      slug: p.slug,
+      stock: p.stock,
+      tags: p.tags.join(","),
+      title: p.title,
+      type: p.type,
+      user: reynaldo.id,
+    };
+
+    queries.push(db.insert(Product).values(product));
+
+    p.images.forEach((img) => {
+      const image = {
+        id: UUID(),
+        image: img,
+        productId: product.id,
+      };
+
+      queries.push(db.insert(ProductImage).values(image));
+    });
+  });
+
+  await db.batch(queries);
 }
