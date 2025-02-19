088fa59d770a90de67ee4448ee8aaacee8dc7643
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2025-02-19 05:03:24 -0400

    Merge 2025-02-19

--- src/actions/products/get-products-by-page.action.ts
+++ src/actions/products/get-products-by-page.action.ts
@@ -34,8 +34,6 @@ export const getProductsByPage = defineAction({
 
     const { rows } = await db.run(productsQuery);
 
-    console.log(rows);
-
     // const products = await db
     //   .select()
     //   .from(Product)
