6fcec31619f05035c4adb8b77661a966205a2a5e
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2025-02-21 08:56:07 -0400

    Se creó un nano store para el contador del navbar

--- src/components/cart/CartCounter.tsx
+++ src/components/cart/CartCounter.tsx
@@ -1,8 +1,20 @@
+import { itemsInCart } from "@/store";
+import { CartCookiesClient } from "@/utils";
+import { useStore } from "@nanostores/react";
+import { useEffect } from "react";
+
 export const CartCounter = () => {
+  const $itemsInCart = useStore(itemsInCart);
+
+  useEffect(() => {
+    const cart = CartCookiesClient.getCart();
+    itemsInCart.set(cart.length);
+  }, []);
+
   return (
     <a href="/cart" className="relative inline-block">
       <span className="absolute -top-2 -right-2 flex justify-center items-center bg-blue-600 text-white text-xs rounded-full w-5 h-5">
-        3
+        {$itemsInCart}
       </span>
 
       <svg
