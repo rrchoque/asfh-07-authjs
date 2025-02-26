d05ee5e49c4296129721d0c50b4a78e7feca785e
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2025-02-26 06:08:58 -0400

    Se agregó la configuración para usar base de datos remota

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,17 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import netlify from "@astrojs/netlify";
 
-import db from "@astrojs/db";
-
 import auth from "auth-astro";
 
 import react from "@astrojs/react";
+
+import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: netlify(),
-  integrations: [db(), auth(), react()],
+  integrations: [auth(), react(), db()],
 
   vite: {
     plugins: [tailwindcss()],
